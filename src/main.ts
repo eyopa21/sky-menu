1519b6f61b633a58cb77a4cb89f7ddbf4ca86a4a
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-10 22:27:33 +0300

    feat: add ClassSerializerInterceptor to main application bootstrap so that @Exclude on user.entity will be good

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +12,7 @@ async function bootstrap() {
       enableImplicitConversion: true
     }
   }))
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)))
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
