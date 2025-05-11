22fe7ff0ef374d36d10e6054ca4e49ba0eb70b64
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-11 19:19:20 +0300

    feat: add @IsNotEmpty validation to user DTO fields

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,20 +1,24 @@
 import { Type } from "class-transformer";
-import { IS_DATE, IS_DATE_STRING, IsDate, IsEnum, IsString, MinLength } from "class-validator";
+import { IS_DATE, IS_DATE_STRING, IsDate, IsEnum, IsNotEmpty, IsString, MinLength } from "class-validator";
 import { SEX } from "../constants/sex.enum";
 
 
 export class CreateUserDto {
     @IsString()
+    @IsNotEmpty()
     readonly full_name: string
 
     @IsString()
+    @IsNotEmpty()
     readonly email: string
 
     @IsString()
     @MinLength(10)
+    @IsNotEmpty()
     readonly phone_number: string
 
     @MinLength(6)
+    @IsNotEmpty()
     readonly password: string
 
     
