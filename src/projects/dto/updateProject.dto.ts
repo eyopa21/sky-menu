e0eb046ac129062b228d411e441a358f354eb5ac
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-12 12:18:46 +0300

    feat: add UpdateProjectDto with validation for project updates

---
+++ src/projects/dto/updateProject.dto.ts
@@ -0,0 +1,5 @@
+import { PartialType } from "@nestjs/mapped-types";
+
+import { CreateProjectDto } from "./createProject.dto";
+
+export class UpdateProjectDto extends PartialType(CreateProjectDto) {}
