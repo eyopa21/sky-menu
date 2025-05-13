1b822cc098b98f6cf8dac4f8af1f4f717c12814b
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-13 10:41:32 +0300

    feat: add DataSource configuration for PostgreSQL with Users and Projects entities

---
+++ datasource.ts
@@ -0,0 +1,21 @@
+
+
+
+import { Projects } from "src/projects/entity/projects.entity"
+import { Users } from "src/users/entity/user.entity"
+import { DataSource } from "typeorm"
+
+export default new DataSource({
+    type: "postgres",
+    host: process.env.DB_HOST,
+    port: +process.env.DB_PORT!,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    // entities: ['dist/**/*.entity{.ts,.js}'],
+    // migrations: ['dist/migrations/*{.ts,.js}'],
+    entities: [Users, Projects],
+    migrations: ['./src/migrations/*.ts'],
+
+    migrationsTableName: "migrations",
+})
