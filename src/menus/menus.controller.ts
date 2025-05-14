2322863ac8bd20070b314f788ede1fa3ff440488
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-14 14:36:54 +0300

    feat: Menu CRUD done

--- src/menus/menus.controller.ts
+++ src/menus/menus.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Delete, Param, Patch, Post, Req, UseGuards } from '@nestjs/common';
 import { Request } from 'express';
 
 import { MenusService } from './menus.service';
@@ -7,24 +7,40 @@ import { CreateMenuDto } from './dto/createMenu.dto';
 import { OwnershipGuard } from 'src/common/guard/ownership.guard';
 import { ApplyOwnershipMetadata } from 'src/common/guard/decorators/ownership.decorator';
 import { Projects } from 'src/projects/entity/projects.entity';
+import { Menus } from './entity/menus.entity';
+import { UpdateThemeDto } from './dto/updateTheme.dto';
 
 @Controller('menus')
 export class MenusController {
 
     constructor(
         private readonly menusService: MenusService
-    ){}
+    ) { }
 
-    findAll(@Req() request: Request){
+    findAll(@Req() request: Request) {
         return this.menusService.findAll()
     }
 
     @Post()
-    @ApplyOwnershipMetadata(Projects, 'user')
     @UseGuards(AuthGuard)
-    create(@Body() createMenuDto: CreateMenuDto, @Req() request: Request){
+    create(@Body() createMenuDto: CreateMenuDto, @Req() request: Request) {
         const user = request.user
         return this.menusService.createOne(createMenuDto, user.id)
+    }
+
+    @Patch(':id')
+    @ApplyOwnershipMetadata(Menus, 'project.user')
+    @UseGuards(AuthGuard, OwnershipGuard)
+    update(@Param('id') id: string, @Body() updateThemeDto: UpdateThemeDto) {
+        return this.menusService.updateTheme(+id, updateThemeDto)
+
+    }
+
+    @Delete(':id')
+    @ApplyOwnershipMetadata(Menus, 'project.user')
+    @UseGuards(AuthGuard, OwnershipGuard)
+    remove(@Param('id') id: string){
+        return this.menusService.removeMenu(+id)
     }
 
 }
--- src/menus/menus.service.ts
+++ src/menus/menus.service.ts
@@ -4,37 +4,61 @@ import { Menus } from './entity/menus.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { CreateMenuDto } from './dto/createMenu.dto';
 import { ProjectsService } from 'src/projects/projects.service';
+import { UpdateThemeDto } from './dto/updateTheme.dto';
 
 @Injectable()
 export class MenusService {
-constructor(
-    @InjectRepository(Menus)
-    private readonly menusRepository: Repository<Menus>,
+    constructor(
+        @InjectRepository(Menus)
+        private readonly menusRepository: Repository<Menus>,
 
-    private readonly projectsService: ProjectsService
-){}
+        private readonly projectsService: ProjectsService
+    ) { }
 
-
-    findAll(){
+    findAll() {
         return this.menusRepository.find()
     }
 
-    findMyMenus(projectId: number){
-        return this.menusRepository.findBy({projectId})
+    async findOneById(id: number) {
+        const menu = this.menusRepository.findBy({ id })
+        if (!menu) {
+            throw new NotFoundException('Menu not found')
+        }
+        return menu
     }
 
-    async createOne(createMenuDto: CreateMenuDto, userId: number){
+    findMyMenus(projectId: number) {
+        return this.menusRepository.findBy({ projectId })
+    }
+
+    async createOne(createMenuDto: CreateMenuDto, userId: number) {
         const project = await this.projectsService.findOneById(createMenuDto.projectId)
-        if(!project){
+        if (!project) {
             throw new NotFoundException('Project not found')
         }
-        if(project.userId !== userId){
+        if (project.userId !== userId) {
             throw new ForbiddenException('You do not own this resource')
         }
         const menu = this.menusRepository.create({
             ...createMenuDto
         })
         return this.menusRepository.save(menu)
-        
+
+    }
+
+    async updateTheme(menuId: number, updateThemeDto: UpdateThemeDto) {
+        const menu = await this.menusRepository.preload({
+            id: +menuId,
+            ...updateThemeDto,
+        })
+        if (!menu) {
+            throw new NotFoundException(`Menu not found`)
+        }
+        return this.menusRepository.save(menu)
+    }
+
+    async removeMenu(id: number) {
+        const project = await this.findOneById(id)
+        return this.menusRepository.remove(project)
     }
 }
