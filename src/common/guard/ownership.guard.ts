5bfc113a36cb5c8983ea75876b771736dd7fd6fb
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-14 12:44:33 +0300

    refactor: clean up OwnershipGuard code and fully dynamic guard

--- src/common/guard/ownership.guard.ts
+++ src/common/guard/ownership.guard.ts
@@ -15,13 +15,13 @@ export class OwnershipGuard implements CanActivate {
   constructor(
     private readonly reflector: Reflector,
     private readonly dataSource: DataSource,
-  ) {}
+  ) { }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest();
     const user = request.user;
     const resourceId = request.params.id;
- 
+
 
     if (!user) throw new UnauthorizedException('User not authenticated');
 
@@ -30,30 +30,29 @@ export class OwnershipGuard implements CanActivate {
     const ownershipField = this.reflector.get<string>('ownership_field', context.getHandler());
 
 
-    console.log(user)
-
     if (!entity || !ownershipField) {
       throw new Error('OwnershipGuard requires @CheckOwnership metadata');
     }
 
     const repo = this.dataSource.getRepository(entity);
+
     const resource = await repo.findOne({
       where: { id: +resourceId },
-      // relations: [ownershipField], // eager load the owner relation
+      relations: [ownershipField], // eager load the owner relation
     });
 
-  
+    function getNestedValue(obj: any, path: string): any {
+      return path.split('.').reduce((o, key) => o?.[key], obj);
+    }
+
+
+
     if (!resource) {
       throw new ForbiddenException('Resource not found');
     }
 
-    // const owner = resource[ownershipField];
-    // const ownerId = typeof owner === 'object' ? owner.id : owner;
-    const ownerId =  resource instanceof Users ? resource.id : resource.userId
-
-
-
-
+    const owner = getNestedValue(resource, ownershipField);
+    const ownerId = entity instanceof Users ? resource.id : typeof owner === 'object' ? owner.id : owner;
     if (user.id !== ownerId) {
       throw new ForbiddenException('You do not own this resource');
     }
