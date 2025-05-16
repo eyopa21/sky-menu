1489de0483dc46bf183c726d93f480f1e448c3b9
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-16 15:36:51 +0300

    feat: setting up swagger api documentation

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { TypeOrmExceptionFilter } from './common/filters/typeorm-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -16,6 +17,13 @@ async function bootstrap() {
       },
     }),
   );
+
+  const options = new DocumentBuilder().setTitle('Sky-Menu').setDescription('Digital menu application').setVersion('1.0').build()
+  const document = SwaggerModule.createDocument(app, options)
+  SwaggerModule.setup('api', app, document)
+
+
+
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.useGlobalFilters(new HttpExceptionFilter(), new TypeOrmExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
