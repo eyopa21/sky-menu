00b97ab607b9f8ca99fb3def22498dcf272476f4
Eyob Nigussie <jobtennis21@gmail.com>
2025-05-22 20:40:09 +0300

    test: workflow

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Application is healthy';
+    return 'Application is healthy ci showcase';
   }
 }
